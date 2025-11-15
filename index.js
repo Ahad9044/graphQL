9b2b21af293b0e43625478c40731449307674c8c
Your Name <syd.ahad@gmail.com>
2025-11-15 17:57:45 +0530

    todo api query

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import axios from 'axios';
+
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
+const typeDefs =`#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
@@ -12,14 +14,21 @@ const typeDefs = `#graphql
     title: String
     author: String
   }
+    type Todo {
+    id : Int
+    title : String
+    completed: Boolean
+    }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    getBooks: [Book]
+    getTodos: [Todo]
   }
-`;
+    `
+;
 const books = [
   {
     title: 'The Awakening',
@@ -35,7 +44,8 @@ const books = [
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    getBooks: () => books,
+    getTodos: async ()=> (await axios.get ("https://jsonplaceholder.typicode.com/todos")).data
   },
 };
 // The ApolloServer constructor requires two parameters: your schema
